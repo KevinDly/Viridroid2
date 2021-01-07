2cbe01ad357e681e8565e83c0e89f4933874dbd4
Saibogu <kmdolay@gmail.com>
2021-01-07 04:14:21 -0800

    Added functions to assist with automatically adding and updating points

--- commands.js
+++ commands.js
@@ -3,11 +3,15 @@ const Constants = require('./constants.js');
 
 module.exports =
 {
-    points: function (msg, tokens) { points(msg, tokens) },
+    points: function (msg, tokens, data) { points(msg, tokens, data) },
     bet: function (msg, tokens) {}
 }
 
-function points(msg, tokens) {
+function points(msg, tokens, data) {
     var guildID = msg.guild.id
     var memberID = msg.member.id
+    var dataKey = `${guildID} - ${memberID}`
+    var points = data.get(dataKey, "points");
+    var author = msg.author.toString()
+    msg.channel.send(`${author} has: ${points} points!`)
 }
--- index.js
+++ index.js
@@ -7,51 +7,21 @@ const fs = require('fs')
 
 const client = new Discord.Client()
 
-/*BIG TODO: Write a promise that promises that the variable that holds the 
- * data for the user (in client.points) is secured.
- * After the promise is fufilled, run all the event handlers for client as normal.
- * https://stackoverflow.com/questions/48923024/node-js-wait-for-all-files-read-in-the-loop
- * Read more about promsify
- */
+//https://gist.github.com/koad/316b265a91d933fd1b62dddfcc3ff584
 //TODO: Create scheduler to schedule periodic point updates.
 //TODO: Get collection of all guilds
-
-/*USE ENMAP OR SQLITE FOR DATABASE STORAGE*/
-
-/*
- * Iterate through all guilds and all clients
- * Initalize an enmap into client.points
- * Store all into client.points
- * '${userid}-${serverid}':{
- *	serverid:
- *	userid:
- *	points:
- * }
- * Check for a client join every time and ensure points.
- *	client.on("playerjoin" or something)
- *	
- *
- */
 
 
 //Create function to get the userEnmapInfo
 client.isLoaded = false
 
-async function iterate(result) {
-	const data = result
+//Add lambada function
+async function iterate(data, callback) {
 	client.guilds.cache.forEach(function (guild, key) {
 		//Iterate through all members in the guild the client is currently looking at.
-		var guildID = guild.id;
 		guild.members.cache.forEach(function (member) {
 			//Check if member is a bot
-			if (!member.user.bot) {
-				var memberID = member.id
-				data.ensure(`${guildID} - ${memberID}`, {
-					member: memberID,
-					guild: guildID,
-					points: 0
-				})
-			}
+			callback(guild, member, data)
 		})
 	})
 }
@@ -60,14 +30,28 @@ async function getUserInfo() {
 	var promise = new Promise(async function (resolve, reject) {
 		try {
 			var result;
+
+			//Check if data is trying to be loaded again
+			//On first load ever load from persistant storage, on subsequent calls load from client.userData
 			if (!client.isLoaded) {
 				result = new Enmap({ name: 'userdata' })
+
 				//Grabbing data
 				await result.defer
 
-
 				//Iterate through all guilds the client is currently in
-				await iterate(result)
+				await iterate(result, function (guild, member, data) {
+					var guildID = guild.id
+					var memberID = member.id
+					if (!member.user.bot) {
+						var memberID = member.id
+						data.ensure(`${guildID} - ${memberID}`, {
+							member: memberID,
+							guild: guildID,
+							points: 0
+						})
+					}
+				})
 
 				client.userData = result;
 				client.isLoaded = true
@@ -86,14 +70,35 @@ async function getUserInfo() {
 	return promise
 }
 
+function awardPoints(guild, member, data) {
+	var guildID = guild.id
+	var memberID = member.id
+	var dataKey = `${guildID} - ${memberID}`
+	if (!member.user.bot) {
+		var memberID = member.id
+		data.ensure(dataKey, {
+			member: memberID,
+			guild: guildID,
+			points: 0
+		})
+		console.log("Awarding points")
+		data.math(dataKey, "+", 100, "points")
+	}
+}
+
 client.login(Constants.LOGIN_TOKEN)
 
 client.on('ready', async () => {
 	//Iterate through all servers and users and check client.points.
 	await getUserInfo()
+
+	getUserInfo().then(function (result) {
+		setInterval(function () {
+			iterate(result, awardPoints)
+        }, 60000)
+	})
+	
 	console.log("Ready")
-
-
 });
 
 client.on('message', async (msg) => {
@@ -109,18 +114,19 @@ client.on('message', async (msg) => {
 
 		//Check table of functions
 		try {
-			Commands[command](msg, tokens)
+			//Commands[command](msg, tokens, data)
+			getUserInfo().then((result) => {
+				Commands[command](msg, tokens, result)
+            })
 			console.log(Constants.COMMAND_SUCCESS_MESSAGE)
 		}
 		catch (err) {
 			//incorrect command message
+			console.log(err)
 			console.log(Constants.COMMAND_FAILURE_MESSAGE)
 		}
 	}
 });
-
-//Runs all the eventhandlers.
-
 
 process.on('exit', () => {
 	client.destroy()
